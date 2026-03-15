7ccbfa66c81922e47d621c5ce1a459dac200f4d7
Mariana <marianamoskalyk526@gmail.com>
2026-03-15 11:07:03 +0200

    додано функцію з навігацією та кнопкою

---
+++ sto-booking/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="logo">ТУТКА ТАМКА</div>
+
+      <nav>
+        <a href="#">Контакти</a>
+        <a href="#">Служба підтримки</a>
+      </nav>
+
+      <button className="service-btn">Обрати послугу</button>
+    </header>
+  );
+}
+
+export default Navbar;
