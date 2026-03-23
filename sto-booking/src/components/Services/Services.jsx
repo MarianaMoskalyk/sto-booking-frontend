ca51fc378d9092f7a1837fa7c868170c64f1db21
Mariana <marianamoskalyk526@gmail.com>
2026-03-23 21:19:46 +0200

    додано і змінено фото карток

--- sto-booking/src/components/Services/Services.jsx
+++ sto-booking/src/components/Services/Services.jsx
@@ -2,17 +2,24 @@ import { useState } from "react";
 import "./Services.css";
 import BookingModal from "../BookingModal/BookingModal";
 
-import imgService from "../../assets/img/service.png";
+import img1 from "../../assets/img/1.png";
+import img2 from "../../assets/img/2.png";
+import img3 from "../../assets/img/3.png";
+import img4 from "../../assets/img/4.png";
+import img5 from "../../assets/img/5.png";
+import img6 from "../../assets/img/6.png";
+import img7 from "../../assets/img/7.png";
+import img8 from "../../assets/img/8.png";
 
 const services = [
-  { id: 1, title: "Діагностика ходової", img: imgService },
-  { id: 2, title: "Заміна масла та фільтрів", img: imgService },
-  { id: 3, title: "Ремонт гальм. системи", img: imgService },
-  { id: 4, title: "Комп'ютерна діагностика", img: imgService },
-  { id: 5, title: "Заміна ременів ГРМ", img: imgService },
-  { id: 6, title: "Ремонт двигунів", img: imgService },
-  { id: 7, title: "Шиномонтаж і балансування", img: imgService },
-  { id: 8, title: "Обслуговування кондиціонера", img: imgService },
+  { id: 1, title: "Діагностика ходової", img: img1 },
+  { id: 2, title: "Заміна масла та фільтрів", img: img2 },
+  { id: 3, title: "Ремонт гальм. системи", img: img3 },
+  { id: 4, title: "Комп'ютерна діагностика", img: img4 },
+  { id: 5, title: "Заміна ременів ГРМ", img: img5 },
+  { id: 6, title: "Ремонт двигунів", img: img6 },
+  { id: 7, title: "Шиномонтаж і балансування", img: img7 },
+  { id: 8, title: "Обслуговування кондиціонера", img: img8 },
 ];
 
 function Services() {
