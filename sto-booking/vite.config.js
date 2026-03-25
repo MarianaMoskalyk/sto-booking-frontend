da92bb57163b2973d26e7b6b41086e3ef3bc7603
Mariana <marianamoskalyk526@gmail.com>
2026-03-25 10:39:08 +0200

    fix: add base path for vite config

--- sto-booking/vite.config.js
+++ sto-booking/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  // НАЙВАЖЛИВІШИЙ РЯДОК:
   base: "/sto-booking-frontend/",
   plugins: [react()],
 });
